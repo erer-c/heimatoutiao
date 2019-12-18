12b4ec20bd1428aa4330f2a14c8917dd6058bbf6
unknown <1799684417@qq.com>
2019-12-18 21:01:11 +0800

    整理目录-准备资源

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import elementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import './styles/index.less'
+Vue.use(elementUI)
 Vue.config.productionTip = false
 
 new Vue({

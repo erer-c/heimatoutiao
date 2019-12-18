0d6a127b130445e75d591a425cbd32301635a907
unknown <1799684417@qq.com>
2019-12-18 21:12:35 +0800

    页面分析-新建登录和主页

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import login from '../views/login/index.vue'
+import home from '../views/home/index.vue'
 
 Vue.use(VueRouter)
 
 export default new VueRouter({
-
+  routes: [{
+    path: '/',
+    redirect: '/login'
+  }, {
+    path: '/login',
+    component: login
+  }, {
+    path: '/home',
+    component: home
+  }]
 })

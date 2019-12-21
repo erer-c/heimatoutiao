5081c5e2287c0af4ef0f084c89cd22b212aba087
unknown <1799684417@qq.com>
2019-12-21 22:44:23 +0800

    面包屑组件封装

---
+++ src/components/common/bread-crumb.vue
@@ -0,0 +1,18 @@
+<template>
+  <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{path:'/home'}">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>
+        <slot name="nav"></slot>
+      </el-breadcrumb-item>
+  </el-breadcrumb>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import layoutAside from '../components/home/layout-aside.vue'
 import layoutHeader from '../components/home/layout-header.vue'
+import breakcrumb from './common/bread-crumb.vue'
 
 export default {
   install (Vue) {
     Vue.component('layoutAside', layoutAside)
     Vue.component('layoutHeader', layoutHeader)
+    Vue.component('breakcrumb', breakcrumb)
   }
 }

ed6fe36603ad9d5c28a42cfb005f19c13bc5355f
unknown <1799684417@qq.com>
2019-12-23 18:31:41 +0800

    素材列表-页面结构-tab

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -5,12 +5,20 @@
                 素材管理
             </template>
         </breadcrumb>
+        <el-tabs v-model="activeName">
+            <el-tab-pane label="全部素材" name='all'>全部素材</el-tab-pane>
+            <el-tab-pane label="我的收藏" name='collect'>我的收藏</el-tab-pane>
+        </el-tabs>
     </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      activeName: 'all'
+    }
+  }
 }
 </script>
 

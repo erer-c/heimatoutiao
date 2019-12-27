807972167a8f8e4c4fec4ed3b0b30ab64c8161d8
unknown <1799684417@qq.com>
2019-12-28 00:07:30 +0800

    发布文章-封面图片选择组件上传

--- src/components/publish/pubilshCover.vue
+++ src/components/publish/pubilshCover.vue
@@ -6,11 +6,23 @@
           <img @click="clickCurrentImg(item.url)" :src="item.url" alt />
         </el-card>
       </div>
-      <el-row type='flex' justify='center'>
-        <el-pagination background layout="prev, pager, next" :total="page.total" :page-size='page.pageSize' :current-page='page.nowPage' @current-change='changePage'></el-pagination>
+      <el-row type="flex" justify="center">
+        <el-pagination
+          background
+          layout="prev, pager, next"
+          :total="page.total"
+          :page-size="page.pageSize"
+          :current-page="page.nowPage"
+          @current-change="changePage"
+        ></el-pagination>
       </el-row>
     </el-tab-pane>
-    <el-tab-pane label="上传图片" name="uploadImg">配置管理</el-tab-pane>
+    <el-tab-pane label="上传图片" name="uploadImg">
+      <!-- 上传图片组件 -->
+      <el-upload class="avatar-uploader" action :show-file-list="false" :http-request='userDefined'>
+        <i class="el-icon-plus avatar-uploader-icon"></i>
+      </el-upload>
+    </el-tab-pane>
   </el-tabs>
 </template>
 
@@ -28,9 +40,21 @@ export default {
     }
   },
   methods: {
+    //   自定义上传
+    userDefined (params) {
+      let data = new FormData()
+      data.append('image', params.file)
+      this.$axios({
+        url: '/user/images',
+        method: 'post',
+        data
+      }).then(res => {
+        this.$emit('forFather', res.data.url)
+      })
+    },
     // 点击选中图片触发
     clickCurrentImg (url) {
-    //   alert(url)
+      //   alert(url)
       // 点击图片是把图片地址传给显示的封面
       this.$emit('forFather', url)
     },
@@ -42,13 +66,17 @@ export default {
     getMaterial () {
       this.$axios({
         url: '/user/images',
-        params: { collect: false, page: this.page.nowPage, per_page: this.page.pageSize }
+        params: {
+          collect: false,
+          page: this.page.nowPage,
+          per_page: this.page.pageSize
+        }
       }).then(res => {
         // alert(res.data.results.id)
         this.list = res.data.results
         this.page.total = res.data.total_count
-        this.page.nowPage = res.data.page// 当前页数
-        this.page.pageSize = res.data.per_page// 每页数量
+        this.page.nowPage = res.data.page // 当前页数
+        this.page.pageSize = res.data.per_page // 每页数量
       })
     }
   },
@@ -72,5 +100,15 @@ export default {
       height: 100%;
     }
   }
+}
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  line-height: 178px;
+  text-align: center;
+  border: 1px dashed #ccc;
+  border-radius: 5px;
 }
 </style>
